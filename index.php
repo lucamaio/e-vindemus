6ba77f2d9f737daf7a7f790c45d556d0220b03be
lucamaio <lucos.maio@gmail.com>
2026-02-24 09:28:08 +0100

    Preparazione ambiente svilupppo Single Prodotto. Creazione delle cartelle.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <?php
     // var_dump(get_stylesheet_uri());
     get_template_part('template-parts/home/carousel', 'home');
+    get_template_part('template-parts/home/mostra-articoli', 'home');
   ?>
 </main>
 
---
+++ single-prodotto.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Prodotto template file.
+ * 
+ *  In fase di sviluppo, questo file è un placeholder per il template del singolo prodotto.
+ *  In futuro, potrebbe essere arricchito con funzionalità specifiche per i prodotti, come campi personalizzati, layout dedicati, o integrazioni con plugin di e-commerce. 
+ */
